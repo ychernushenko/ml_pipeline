30880d3e4e9f9d969ec722bf35ca71dd5c66b9c8
Iurii Chernushenko <ychernushenko@squareup.com>
2023-11-14 12:49:25 +0100

    initial commit

---
+++ ml_pipeline.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+def load_data(file_path, names):
+    """
+    Load data from TSV files into a pandas DataFrame.
+    """
+    try:
+        return pd.read_csv(file_path, encoding='utf8', sep='\t', names=names)
+    except Exception as e:
+        print(f"Error loading {file_path}: {e}")
+        return None
+
+def preprocess_data(data_root_dir):
+    """
+    Preprocess product and review data.
+    """
+    product_files = [f'{data_root_dir}products-data-{i}.tsv' for i in range(4)]
+    review_files_order_1 = [f'{data_root_dir}reviews-{i}.tsv' for i in range(4) if i != 2]
+    review_files_order_2 = [f'{data_root_dir}reviews-{i}.tsv' for i in [2]]
+
+    products_df = pd.concat([load_data(f, ["id", "category", "product_title"]) for f in product_files], ignore_index=True)
+    reviews_df_order_1 = pd.concat([load_data(f, ["id", "rating", "review_text"]) for f in review_files_order_1], ignore_index=True)
+    reviews_df_order_2 = pd.concat([load_data(f, ["rating", "id", "review_text"]) for f in review_files_order_2], ignore_index=True)
+    reviews_df = pd.concat([reviews_df_order_1, reviews_df_order_2], ignore_index=True)
+
+    return pd.merge(products_df, reviews_df, on="id")
+
+def build_model():
+    """
+    Build a machine learning pipeline.
+    """
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('review_text', TfidfVectorizer(), 'review_text'),
+            ('product_title', TfidfVectorizer(), 'product_title'),
+            ('rating', StandardScaler(), ['rating'])
+        ],
+        remainder='passthrough'
+    )
+
+    return Pipeline([
+        ('preprocessor', preprocessor),
+        ('classifier', LogisticRegression())
+    ])
+
+def main():
+    data_root_dir = '/content/drive/MyDrive/Colab Notebooks/dataset/'
+    df = preprocess_data(data_root_dir)
+
+    features = ['review_text', 'product_title', 'rating']
+    target = 'category'
+
+    X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.2, random_state=42)
+
+    model = build_model()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(f'Accuracy: {accuracy:.2f}')
+
+    print('\nClassification Report:')
+    print(classification_report(y_test, predictions))
+
+if __name__ == "__main__":
+    main()
